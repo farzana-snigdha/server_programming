ef48db6feb3e85fe40e4b0150431abadbe93559c
farzana-snigdha <52860135+farzana-snigdha@users.noreply.github.com>
2021-04-30 14:44:48 +0600

    global variables

---
+++ Lab_1_180042119/globalVariable.js
@@ -0,0 +1,17 @@
+//__dirname - path to the current directory.
+const currentDirectory = __dirname;
+console.log(currentDirectory);
+
+//__filename = filename.
+const fileName = __filename;
+console.log(fileName);
+
+//require - function to use modules.
+const fs = require("fs");
+console.log(fs);
+
+//module - info about current module.
+console.log(module);
+
+//process - info about where the program is being executed.
+console.log(process);
