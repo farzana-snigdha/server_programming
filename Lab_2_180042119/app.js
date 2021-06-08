f2c483a56ccf313b0b931013238d68b0aeead2cc
farzana-snigdha <52860135+farzana-snigdha@users.noreply.github.com>
2021-06-08 21:22:46 +0600

    set cookie

--- Lab_2_180042119/app.js
+++ Lab_2_180042119/app.js
@@ -1,10 +1,23 @@
-const express = require('express')
-const app=express()
+const express = require("express");
+const app = express();
 
-app.get('/', (req,res)=>{
-    res.status(201).send("<h1>hi</h1>")
-    })
-   
+app.get("/", (req, res) => {
+  res.status(201).send("<h1>hi</h1>");
+});
 
+app.get("/about", (req, res) => {
+  res.cookie("username", "asd");
+  // res.clearCookie("username")
+  res.append("key","boo")
+  res.status(201).send("<h1>about</h1>");
+});
 
-module.exports=app
+app.get("/contact", (req, res) => {
+  res.status(201).send("<h1>contact</h1>");
+});
+
+app.use((req, res) => {
+  res.status(401).send("<h1>page not found</h1>");
+});
+
+module.exports = app;
