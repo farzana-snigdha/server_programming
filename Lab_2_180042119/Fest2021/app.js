e47bf4127a4f518f7cfc8df5a4f579ecbc278637
farzana-snigdha <52860135+farzana-snigdha@users.noreply.github.com>
2021-07-29 12:58:22 +0600

    Update app.js

--- Lab_2_180042119/Fest2021/app.js
+++ Lab_2_180042119/Fest2021/app.js
@@ -45,8 +45,11 @@ app.use(express.urlencoded({ extended: false }));
 //Routes
 const indexRoutes = require("./routes/index.routes");
 const userRoutes = require("./routes/users.routes");
+const MORoutes=require('./routes/mathOlympiad.routers')
 app.use(indexRoutes);
 app.use("/users", userRoutes);
+app.use('/MathOlympiad',MORoutes)
+
 //Static Resources
 app.use(express.static("public"));
 module.exports = app;
