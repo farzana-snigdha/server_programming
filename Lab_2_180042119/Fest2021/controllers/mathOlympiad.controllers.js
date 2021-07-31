0b07043c3048a88796264faa60828402e320eaea
farzana-snigdha <52860135+farzana-snigdha@users.noreply.github.com>
2021-07-31 19:16:44 +0600

    participants can be saved in db

--- Lab_2_180042119/Fest2021/controllers/mathOlympiad.controllers.js
+++ Lab_2_180042119/Fest2021/controllers/mathOlympiad.controllers.js
@@ -1,23 +1,67 @@
-const MathOlympiad=require('../models/MathOlympiad.models')
- 
-const getMO= (req,res)=>{
-    res.render('math-olympiad/register.ejs')
-}
+const MathOlympiad = require("../models/MathOlympiad.models");
 
-const postMO= (req,res)=>{
-    const{name,category,contact,email,institution,tshirt}=req.body
-    console.log(institution)
-  
-    res.render('math-olympiad/register.ejs')
-}
+const getMO = (req, res) => {
+  res.render("math-olympiad/register.ejs", { error: req.flash("error") });
+};
 
-const getMOList= (req,res)=>{
-    res.render('math-olympiad/list.ejs')
-}
-const deleteMO= (req,res)=>{
-    const id=req.params.id
-    console.log('id ',id)
-    // res.render('math-olympiad/register.ejs')
-}
+const postMO = (req, res) => {
+  const { name, category, contact, email, institution, tshirt } = req.body;
+  console.log(institution);
+  let registrationFee = 0;
+  if (category == "School") {
+    registrationFee = 250;
+  } else if (category == "College") {
+    registrationFee = 400;
+  } else {
+    registrationFee = 500;
+  }
+  const total = registrationFee;
+  const paid = 0;
+  const selected = false;
+  let error = "";
 
-module.exports={getMO,postMO,getMOList,deleteMO}
+  MathOlympiad.findOne({ name: name, contact: contact }).then((participant) => {
+    if (participant) {
+      error = "Participant with same name and contact exists";
+
+      req.flash("error", error);
+      res.redirect("/MathOlympiad/register");
+    } else {
+      const participant = new MathOlympiad({
+        name,
+        category,
+        contact,
+        email,
+        institution,
+        paid,
+        total,
+        selected,
+        tshirt,
+      });
+      participant
+        .save()
+        .then(() => {
+          error = "Participant has been registered successfully!!";
+          req.flash("error", error);
+          res.redirect("/MathOlympiad/register");
+        })
+        .catch(() => {
+          error = "Unexpected error";
+          req.flash("error", error);
+          res.redirect("/MathOlympiad/register");
+        });
+    }
+  });
+  //   res.render('math-olympiad/register.ejs')
+};
+
+const getMOList = (req, res) => {
+  res.render("math-olympiad/list.ejs");
+};
+const deleteMO = (req, res) => {
+  const id = req.params.id;
+  console.log("id ", id);
+  // res.render('math-olympiad/register.ejs')
+};
+
+module.exports = { getMO, postMO, getMOList, deleteMO };
