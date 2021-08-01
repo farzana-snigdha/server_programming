651ed59b0bd482f65e6ba9f2fc01734382229dd6
farzana-snigdha <52860135+farzana-snigdha@users.noreply.github.com>
2021-08-01 13:37:57 +0600

    prog content payment status,select fn implemented

--- Lab_2_180042119/Fest2021/routes/progContest.routes.js
+++ Lab_2_180042119/Fest2021/routes/progContest.routes.js
@@ -8,10 +8,10 @@ const {getPC,postPC,getPCList,deletePC,paymentDonePC,selectPC, getEditPC,postEdi
 router.get('/register',ensureAuthenticated,addUserData,getPC)
 router.post('/register',ensureAuthenticated,addUserData,postPC)
 router.get('/list',ensureAuthenticated,addUserData,getPCList)
-router.get('/delete/:id',ensureAuthenticated,addUserData,deletePC)
-router.get('/paymentDone/:id',ensureAuthenticated,addUserData,paymentDonePC)
-router.get('/select/:id',ensureAuthenticated,addUserData,selectPC)
-router.get('/edit-participant/:id',ensureAuthenticated,addUserData,getEditPC)
-router.post('/edit-participant',ensureAuthenticated,addUserData,postEditPC)
+router.get('/pc-delete/:id',ensureAuthenticated,addUserData,deletePC)
+router.get('/pc-paymentDone/:id',ensureAuthenticated,addUserData,paymentDonePC)
+router.get('/pc-select/:id',ensureAuthenticated,addUserData,selectPC)
+router.get('/pc-edit-participant/:id',ensureAuthenticated,addUserData,getEditPC)
+router.post('/pc-edit-participant',ensureAuthenticated,addUserData,postEditPC)
 
 module.exports=router
