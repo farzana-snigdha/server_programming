538fa372f8b4eda526d6873a00abceb4d9b74152
farzana-snigdha <52860135+farzana-snigdha@users.noreply.github.com>
2021-09-02 08:46:05 +0600

    Update sendMail.Controllers.js

--- Lab_2_180042119/Fest2021/controllers/sendMail.Controllers.js
+++ Lab_2_180042119/Fest2021/controllers/sendMail.Controllers.js
@@ -8,11 +8,11 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-const sendConfirmationMail = (reciever, topic,name) => {
+const sendConfirmationMail = (reciever, topic,name,confirmationMail) => {
   let mailOptions = {
     to: reciever,
     subject: `Confirmation for ${topic} event in FEST2021`,
-    html: `Congratulations ${name}!! You have successfully registered in FEST2021  ${topic} Event. Best of Luck!!!`,
+    html: `Congratulations ${name}!! You have successfully registered in FEST2021  ${topic} Event. Your ID: ${confirmationMail} Best of Luck!!!`,
   };
   transporter.sendMail(mailOptions, function (error) {
     if (error) {
